4e10a1e85b23be9ef4a9a0bcb975b4c7ac410181
Mursel <murselcetin51@gmail.com>
2022-01-24 22:47:35 +0300

    İlk proje

---
+++ app/src/main/java/com/mursel/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mursel.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mursel.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var islem: islemler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        islem = islemler()
+        toplamaIslemi()
+    }
+
+    fun toplamaIslemi() {
+        binding.topla.setOnClickListener {
+            val sayi1 = binding.input1.text
+            val sayi2 = binding.input2.text
+            val sonuc = islem.topla(sayi1.toString().toInt(), sayi2.toString().toInt())
+            binding.sonuc.text = "Sonuç= "+sonuc.toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/mursel/myapplication/islemler.kt
@@ -0,0 +1,19 @@
+package com.mursel.myapplication
+
+import android.view.View
+
+class islemler {
+
+    fun topla(s1:Int,s2:Int):Int{
+        return s1+s2
+    }
+    fun cikar(a:Int,b:Int):Int{
+        return a-b
+    }
+    fun carp(x:Int,y:Int):Int{
+        return x*y
+    }
+    fun bol(q:Int,w:Int): Int {
+        return q/w
+    }
+}
